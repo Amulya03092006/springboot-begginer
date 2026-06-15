44c9726c4d42672f4841d6d9a18d74354e7aefb3
Kothapalli Amulya <kothapalliamulyareddy03@gmail.com>
2026-06-16 01:17:59 +0530

    Integrated HTML and CSS along with springboot project

---
+++ src/main/java/com/amulya/springboot_begginer/SpringbootBegginerApplication.java
@@ -0,0 +1,13 @@
+package com.amulya.springboot_begginer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBegginerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBegginerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amulya/springboot_begginer/SpringbootBegginerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amulya.springboot_begginer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBegginerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
